233ec41b5d39f515edce3e388236cfd421824d0f
Dermot Burke <dermotburke@Dermots-MBP-2.home>
2023-03-16 14:01:48 +0000

    update article

--- run.sh
+++
@@ -1,6 +0,0 @@
-docker-compose up -d
-./init.sh
-mvn clean package
-java -jar pizza-shop-repository/target/*.jar &
-java -jar pizza-shop-web-receiver/target/*.jar &
-java -jar pizza-shop-transformer/target/*.jar &
